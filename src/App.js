3a84a9e87db221e2dc606f33b3ed5bf0ac83b80f
Shekhorroy100 <shekhorroy100@gmail.com>
2023-02-08 17:45:06 +0530

    debashish/Make_Config_Changes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import "./App.css";
 import {Route, Routes} from "react-router-dom";
 import Login from './pages/login/login';
 import Dashboard from "./pages/dashboard/dashboard";
-import {AppContextProvider} from "./context/AppContext";
 import {useEffect, useState} from "react";
 import {BaseRoute, getFromLocalStorage, LOCAL_STORAGE_KEYS} from "./config/utils";
 import {RouteContextProvider} from "./context/RouteContext";
-
+import {BUILD_TYPE} from "./config/config";
 function App() {
   const[authKey, setAuthKey] = useState(null)
+  console.log("BUILD TYPE: ", BUILD_TYPE)
   useEffect(()=>{
       const key = getFromLocalStorage(LOCAL_STORAGE_KEYS.AUTH_TOKEN)
       console.log("key: ", key)
