fd5ac59b061e57db4da5ce37a41befb8a129da3b
Shekhorroy100 <shekhorroy100@gmail.com>
2023-02-09 16:56:34 +0530

    Debashish/Added_Style_Sidebar

--- src/pages/auditTable/AuditTable.jsx
+++ src/pages/auditTable/AuditTable.jsx
@@ -7,58 +7,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import {Typography} from "@mui/material";
-import IconButton from "@mui/material/IconButton";
 import {convertEpochToDateAndTime} from "../../config/utils";
-
-// const columns = [
-//     { id: 'name', label: 'Name', minWidth: 170 },
-//     { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-//     {
-//         id: 'population',
-//         label: 'Population',
-//         minWidth: 170,
-//         align: 'right',
-//         format: (value) => value.toLocaleString('en-US'),
-//     },
-//     {
-//         id: 'size',
-//         label: 'Size\u00a0(km\u00b2)',
-//         minWidth: 170,
-//         align: 'right',
-//         format: (value) => value.toLocaleString('en-US'),
-//     },
-//     {
-//         id: 'density',
-//         label: 'Density',
-//         minWidth: 170,
-//         align: 'right',
-//         format: (value) => value.toFixed(2),
-//     },
-// ];
-
-function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-}
-
-const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
-];
-
 
 function AuditTable({
     data = [],
@@ -70,7 +19,7 @@ function AuditTable({
                     <TableHead>
                         <TableRow>
                             <TableCell align="left"
-                                       colSpan={Object.keys(data[0]).length}
+                                       colSpan={data && data[0] ? Object.keys(data[0]).length : 100}
                                        style={{backgroundColor:'#1976d2', color: 'white', borderTopLeftRadius: 5, borderTopRightRadius: 5 }}>
                                     <Typography fontSize="h6">
                                         Audit Log
@@ -104,7 +53,7 @@ function AuditTable({
                                                     return (
                                                         <TableCell key={column+index} align="left">
                                                             {
-                                                                convertEpochToDateAndTime(value)
+                                                                value && convertEpochToDateAndTime(value)
                                                             }
                                                         </TableCell>
                                                     );
